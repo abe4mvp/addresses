d2d8d5e571093def777571a028990da2d8f1919f
Abe Schonfeld <abe.schonfeld@gmail.com>
2014-01-24 02:14:33 -0500

    added SQL comment

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -18,9 +18,23 @@ class CustomersController < ApplicationController
   def search
     @postal_code = params[:postal_code]
     @customers = Customer.includes(:addresses).merge(Address.where(postal_code: @postal_code ))
+    #this is a active_record shorcut for the following sql code:
     
+    # SELCTECT
+ #      customers, adresses 
+ #    FROM
+ #      customers
+ #    LEFT OUTTER JOIN
+ #      adresses 
+ #    ON 
+ #      customer.id = address.customer_id
+ #    WHERE
+ #      adresses.postal_code == ?
+       
     
     render :search_results
   end
+  
+  
   
 end
